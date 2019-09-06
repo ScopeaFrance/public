9395779c36131d3dd0a113d1b0900528549c4674
Marc Aubry <marc.aubry@scopea.fr>
2019-09-06 09:59:31 +0200

    switch back to master branch

--- public.sh
+++ public.sh
@@ -10,7 +10,7 @@ apt upgrade -y
 apt install git vim -y
 
 echo "Clonning infra scripts"
-git clone -b ubuntu https://github.com/ScopeaFrance/infra.git 
+git clone https://github.com/ScopeaFrance/infra.git 
 
 echo "Installing keys for ssh access"
 cp infra/ssh-keys.txt .ssh/authorized_keys
